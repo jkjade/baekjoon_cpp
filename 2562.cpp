afb2329d4c34df394e00fb29f7218dbc97c6dd69
jkjade <jkjade@naver.com>
2024-09-16 10:35:36 +0900

    2562

---
+++ 2562.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int arr[9];
+    int max = 0, max_index = 0;
+    for(int i = 0; i < 9; i++){
+        cin >> arr[i];
+        if(max < arr[i]){
+            max = arr[i];
+            max_index = i;
+        }
+    } 
+    cout << max << "\n" << max_index + 1;
+    return 0;
+}
