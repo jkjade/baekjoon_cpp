c4383207f409ba4ad51cc1cffd394f8673545f92
jkjade <jkjade@naver.com>
2024-09-27 20:32:49 +0900

    2935

---
+++ 2935.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string a, b;
+    char op;
+
+    cin >> a >> op >> b;
+
+    switch(op){
+        case '*':
+            cout << "1";
+            for(int i = 0; i < a.length() + b.length() - 2; i++) cout << "0";
+            break;
+        case '+':
+            if(a.length() > b.length()) {
+                a[a.length() - b.length()] = '1';
+                cout << a;
+            }
+            else if(b.length() > a.length()) {
+                b[b.length() - a.length()] = '1';
+                cout << b;
+            }
+            break;
+        default:
+            break;
+    }
+
+}
